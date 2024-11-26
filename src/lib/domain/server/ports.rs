b3965219933abe61fe1887f0b7ddda7c08983113
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-11-26 02:02:24 +0100

    feat: add new method `find_all`

--- src/lib/domain/server/ports.rs
+++ src/lib/domain/server/ports.rs
@@ -1,5 +1,6 @@
 use crate::domain::server::models::server::{Server, ServerError};
 use crate::domain::server::models::server_validator::CreateServer;
+use crate::domain::server::models::status::ServerStatus;
 use std::future::Future;
 
 pub trait ServerService: Clone + Send + Sync + 'static {
@@ -8,6 +9,10 @@ pub trait ServerService: Clone + Send + Sync + 'static {
         server: CreateServer,
     ) -> impl Future<Output = Result<Server, ServerError>> + Send;
     fn find_by_id(&self, id: String) -> impl Future<Output = Result<Server, ServerError>> + Send;
+    fn find_all(
+        &self,
+        status: Option<ServerStatus>,
+    ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
 }
 
 pub trait ServerRepository: Clone + Send + Sync + 'static {
@@ -17,4 +22,8 @@ pub trait ServerRepository: Clone + Send + Sync + 'static {
         name: String,
     ) -> impl Future<Output = Result<Server, ServerError>> + Send;
     fn find_by_id(&self, id: String) -> impl Future<Output = Result<Server, ServerError>> + Send;
+    fn find_all(
+        &self,
+        status: Option<ServerStatus>,
+    ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
 }
