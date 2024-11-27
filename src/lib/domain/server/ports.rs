f8a92dc2519757da1b19ed6dfcc1493a440467b9
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-11-27 23:50:02 +0100

    feat: add new methods in repository and update service

--- src/lib/domain/server/ports.rs
+++ src/lib/domain/server/ports.rs
@@ -1,8 +1,8 @@
 use crate::domain::server::models::server::{Server, ServerError};
+use crate::domain::server::models::server_type::ServerType;
 use crate::domain::server::models::server_validator::CreateServer;
 use crate::domain::server::models::status::ServerStatus;
 use std::future::Future;
-use crate::domain::server::models::server_type::ServerType;
 
 pub trait ServerService: Clone + Send + Sync + 'static {
     fn create_server(
@@ -13,7 +13,7 @@ pub trait ServerService: Clone + Send + Sync + 'static {
     fn find_all(
         &self,
         status: Option<ServerStatus>,
-        server_type: Option<ServerType>
+        server_type: Option<ServerType>,
     ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
 }
 
@@ -24,11 +24,18 @@ pub trait ServerRepository: Clone + Send + Sync + 'static {
         name: String,
     ) -> impl Future<Output = Result<Server, ServerError>> + Send;
     fn find_by_id(&self, id: String) -> impl Future<Output = Result<Server, ServerError>> + Send;
-    fn find_all(
-        &self,
-    ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
+    fn find_all(&self) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
     fn find_by_status(
         &self,
         status: ServerStatus,
+    ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
+    fn find_by_type(
+        &self,
+        server_type: ServerType,
+    ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
+    fn find_by_status_and_type(
+        &self,
+        status: ServerStatus,
+        server_type: ServerType,
     ) -> impl Future<Output = Result<Vec<Server>, ServerError>> + Send;
 }
